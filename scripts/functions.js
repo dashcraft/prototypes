de3c9b0cc8638dd68ae51b7a70e5fe09629b8748
Stewart Hines <stewartwadehines@gmail.com>
2016-06-09 15:30:53 -0500

    Updated stuff
    - Added a thing that adds a disabled class to the live chat button when
    the hours are off
    - Added a confirmation modal
    - updated FAQs
    - made all the changes MC asked for.

--- scripts/functions.js
+++ scripts/functions.js
@@ -44,3 +44,17 @@ $(document).ready(function(){
 
   });
 });
+// = = = = Temporay Class = = = = //
+(function($){
+  $.fn.extend({
+    addTemporaryClass: function(className, duration){
+      var elements = this;
+      setTimeout(function(){
+        elements.removeClass(className);
+      }, duration);
+      return this.each(function(){
+        $(this).addClass(className);
+      });
+    }
+  });
+})(jQuery);
